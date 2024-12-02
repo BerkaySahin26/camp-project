4c5cd3883025c68656a0b2da102832e76027cd6a
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-12-02 22:31:41 +0300

    up jwt

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,9 +4,17 @@ export default class AuthService {
     login(credentials) {
         return axios.post('https://localhost:7131/api/Auth/login', credentials)
             .then((response) => {
-                const token = response.data.token;
-                localStorage.setItem('token', token);
-                axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+                const token = response.data?.data?.token;
+                if (token) {
+
+                    localStorage.setItem('token', token);
+
+
+                    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+                } else {
+                    throw new Error("Token alınamadı!");
+                }
                 return response;
             })
             .catch((error) => {
@@ -17,9 +25,7 @@ export default class AuthService {
 
     register(user) {
         return axios.post('https://localhost:7131/api/Auth/register', user)
-            .then((response) => {
-                return response;
-            })
+            .then((response) => response) // Yanıtı döndür
             .catch((error) => {
                 console.error('Registration error:', error);
                 throw error;
@@ -27,6 +33,13 @@ export default class AuthService {
     }
 
     getToken() {
+
         return localStorage.getItem('token');
+    }
+
+    logout() {
+
+        localStorage.removeItem('token');
+        delete axios.defaults.headers.common['Authorization'];
     }
 }
